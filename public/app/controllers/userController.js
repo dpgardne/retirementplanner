24f8a3c4401a571b66c584d1b638d4cd3feceb14
David Gardner <dpgardner10@gmail.com>
2017-09-20 09:26:49 -0700

    loading works

--- public/app/controllers/userController.js
+++ public/app/controllers/userController.js
@@ -3,14 +3,17 @@ angular.module('userControllers', [])
 .controller('signupController', function($http){
   const app = this;
     this.newUser = function(userData) {
+      app.loading = true;
       app.errorMsg = false;
       console.log('form submitted')
       $http.post('/api/users', this.userData).then(function(data){
           console.log(data.data.success)
           console.log(data.data.message)
           if(data.data.success) {
+            app.loading = false;
             app.successMsg = data.data.message;
           } else {
+            app.loading = false;
             app.errorMsg = data.data.message;
 
           }
