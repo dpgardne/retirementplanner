af8cb66981dba6860bf3c2fbb8900c1fd4b77340
David Gardner <dpgardner10@gmail.com>
2017-09-20 09:34:12 -0700

    user redirect to home works

--- public/app/controllers/userController.js
+++ public/app/controllers/userController.js
@@ -1,18 +1,19 @@
 angular.module('userControllers', [])
 
-.controller('signupController', function($http, $location){
+.controller('signupController', function($http, $location, $timeout){
   const app = this;
     this.newUser = function(userData) {
       app.loading = true;
       app.errorMsg = false;
       console.log('form submitted')
       $http.post('/api/users', this.userData).then(function(data){
-          console.log(data.data.success)
-          console.log(data.data.message)
           if(data.data.success) {
             app.loading = false;
-            app.successMsg = data.data.message;
-            $location.path('/')
+            app.successMsg = data.data.message + '..Redirecting to home';
+            $timeout(function(){
+              $location.path('/')
+            }, 2000);
+
           } else {
             app.loading = false;
             app.errorMsg = data.data.message;
