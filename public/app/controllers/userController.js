f23d0bddab65861aa2301d7e810b7a2c9254c00c
David Gardner <dpgardner10@gmail.com>
2017-09-20 08:55:51 -0700

    successfully created a form to add users to the db

--- public/app/controllers/userController.js
+++ public/app/controllers/userController.js
@@ -1,7 +1,9 @@
 angular.module('userControllers', [])
 
-.controller('signupController', function(){
-    this.newUser = function() {
-      console.log('new button')
+.controller('signupController', function($http){
+    this.newUser = function(userData) {
+      console.log('form submitted')
+      console.log(this.userData)
+      $http.post('/api/users', this.userData);
     }
 })
